2b714c6af28b2fd3ff23a9d7efd184b425c405a4
ikilo9027 <95391806+ikilo9027@users.noreply.github.com>
2022-09-29 17:08:47 +0900

    3진법 뒤집기

---
+++ 22_09/29/2_CT copy.js
@@ -0,0 +1,25 @@
+// 시저 암호
+// 문제 설명
+// 어떤 문장의 각 알파벳을 일정한 거리만큼 밀어서 다른 알파벳으로 바꾸는 암호화 방식을 시저 암호라고 합니다. 예를 들어 "AB"는 1만큼 밀면 "BC"가 되고, 3만큼 밀면 "DE"가 됩니다. "z"는 1만큼 밀면 "a"가 됩니다. 문자열 s와 거리 n을 입력받아 s를 n만큼 민 암호문을 만드는 함수, solution을 완성해 보세요.
+
+// 제한 조건
+// 공백은 아무리 밀어도 공백입니다.
+// s는 알파벳 소문자, 대문자, 공백으로만 이루어져 있습니다.
+// s의 길이는 8000이하입니다.
+// n은 1 이상, 25이하인 자연수입니다.
+
+
+// 첫번째 오답
+function solution(s, n) {
+  let list = Array.from(s).map((val, i) => {
+    var p = val === 'z' | val === 'Z' ? val.charCodeAt(0) - 26 : val.charCodeAt(0)
+    return val.charCodeAt(0) === 32 ? ' ' : String.fromCharCode(p + n)
+  })
+  return list.join('');
+}
+
+// 두번쨰 정답
+function solution(s, n) {
+  var chars = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXY                          "
+  return s.split('').map(e => chars[chars.indexOf(e) + n]).join('');
+}
