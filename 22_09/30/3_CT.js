1d4bf1709918755243f3266c7f810db2a889d91a
ikilo9027 <95391806+ikilo9027@users.noreply.github.com>
2022-09-30 13:04:33 +0900

    update

---
+++ 22_09/30/3_CT.js
@@ -0,0 +1,60 @@
+// 직사각형 별찍기
+// 이 문제에는 표준 입력으로 두 개의 정수 n과 m이 주어집니다.
+// 별(*) 문자를 이용해 가로의 길이가 n, 세로의 길이가 m인 직사각형 형태를 출력해보세요.
+
+// 제한 조건
+// n과 m은 각각 1000 이하인 자연수입니다.
+// 예시
+// 입력
+
+// 5 3
+// 출력
+
+// *****
+// *****
+// *****
+
+// 나의 정답
+process.stdin.setEncoding('utf8');
+process.stdin.on('data', data => {
+  const n = data.split(" ");
+  const a = Number(n[0]), b = Number(n[1]);
+  let result = ""
+  Array.from({ length: b }, (v_i, i) => {
+    Array.from({ length: a }, (v_k, k) => {
+      result += "*"
+    })
+    result += "\n"
+  });
+  console.log(result)
+});
+
+// 깔끔한 정답 1
+process.stdin.setEncoding('utf8');
+process.stdin.on('data', data => {
+  const n = data.split(" ");
+  const a = Number(n[0]), b = Number(n[1]);
+  const row = '*'.repeat(a)
+  for (let i = 0; i < b; i++) {
+    console.log(row)
+  }
+
+});
+
+// 깔끔한 정답 2
+process.stdin.setEncoding('utf8');
+process.stdin.on('data', data => {
+  const n = data.split(" ");
+  const a = Number(n[0]), b = Number(n[1]);
+  const star = `${'*'.repeat(a)}\n`;
+
+  console.log(star.repeat(b));
+});
+
+// 깔끔한 정답 3
+process.stdin.setEncoding('utf8');
+process.stdin.on('data', data => {
+  const n = data.split(" ");
+  const a = Number(n[0]), b = Number(n[1]);
+  console.log((('*').repeat(a) + `\n`).repeat(b))
+});
