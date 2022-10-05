8da99a8295dd9701a0b9920a5ad7bfe41be08f03
ikilo9027 <95391806+ikilo9027@users.noreply.github.com>
2022-10-05 17:56:02 +0900

    update

---
+++ 22_10/05/4_CT.js
@@ -0,0 +1,27 @@
+// 문자열 내림차순으로 배치하기
+// 문자열 s에 나타나는 문자를 큰것부터 작은 순으로 정렬해 새로운 문자열을 리턴하는 함수, solution을 완성해주세요.
+// s는 영문 대소문자로만 구성되어 있으며, 대문자는 소문자보다 작은 것으로 간주합니다.
+
+// 제한 사항
+// str은 길이 1 이상인 문자열입니다.
+// 입출력 예
+// s	return
+// "Zbcdefg"	"gfedcbZ"
+
+// 첫번째 정답
+function solution(s) {
+  return Array.from(s)
+    .map((val) => val.charCodeAt(0))
+    .sort((a, b) => b - a)
+    .map(v => String.fromCharCode(v))
+    .join('');
+}
+
+// 깔끔한 정답
+function solution(s) {
+  return s
+    .split("")
+    .sort()
+    .reverse()
+    .join("");
+}
